3dab6d5e4d575058c8288a534bf40b07c787f951
sise <sise2025@gmail.com>
2026-02-09 22:24:49 -0500

    Se estableció la conexión con la base de datos

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using VeterinariaAppWeb.Models;
+
+namespace VeterinariaAppWeb.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly string cadenaConexion = "server=localhost; database=veterinaria;user id=sa;password=sqladmin;TrustServerCertificate=true";
+        public IActionResult Index()
+        {
+            var listado = obtenerProductos();
+            return View(listado);
+        }
+
+        #region . Métodos privados .
+        private List<Producto> obtenerProductos()
+        {
+            var listado = new List<Producto>();
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+            }
+            return listado;
+        }
+        #endregion
+    }
+}
--- obj/Debug/net10.0/VeterinariaAppWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/VeterinariaAppWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeterinariaAppWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1c9186e50b55fc19243b7f2c2dd3a68a927088")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeterinariaAppWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeterinariaAppWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
