57403bce585322622c954d8fe00176a1ff2978c1
Alex Sullon <alexsullonporras@hotmail.com>
2026-02-11 22:19:56 -0500

    Feat: Se implementó el listado de productos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -6,7 +6,7 @@ namespace VeterinariaAppWeb.Controllers
 {
     public class ProductosController : Controller
     {
-        private readonly string cadenaConexion = "server=localhost; database=veterinaria;user id=sa;password=sqladmin;TrustServerCertificate=true";
+        private readonly string cadenaConexion = "server=localhost; database=veterinaria;Integrated security = true;TrustServerCertificate=true";
         public IActionResult Index()
         {
             var listado = obtenerProductos();
@@ -19,7 +19,28 @@ namespace VeterinariaAppWeb.Controllers
             var listado = new List<Producto>();
             using (var conexion = new SqlConnection(cadenaConexion))
             {
-                conexion.Open();
+                using (var comando = new SqlCommand("SELECT * FROM Productos", conexion))
+                {
+                    conexion.Open();
+                    using (var lector = comando.ExecuteReader())
+                    {
+                        if(lector != null && lector.HasRows)
+                        {
+                            while (lector.Read())
+                            {
+                                listado.Add(new Producto()
+                                {
+                                    ID = lector.GetInt32(0),
+                                    Nombre = lector.GetString(1),
+                                    Descripcion = lector.GetString(2),
+                                    PathImagen = lector.GetString(3),
+                                    Precio = lector.GetDecimal(4),
+                                    CategoriaID = lector.GetInt32(5)
+                                });
+                            }
+                        }
+                    }
+                }
             }
             return listado;
         }
